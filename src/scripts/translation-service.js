9d403cd2f27ba88c3f8b6116829e0db021b372ec
Gurusewak Kalra <gurusewak.kalra@logmein.com>
2018-08-02 16:05:35 -0500

    Distribute all locales files

--- src/scripts/translation-service.js
+++ src/scripts/translation-service.js
@@ -13,7 +13,6 @@ const tranlationService = {
     this.handleBundle();
   },
   extractSentences(htmlData) {
-    tranlationService.sentenceArray = [];
     for (var i = 0; i < htmlData.length; i++) {
       //if there is text tag inside a text tag -> <a>We need this sentence <strong>together</strong></a>
       if (htmlData[i].type === 'element' && (tranlationService.textTags.indexOf(htmlData[i].tagName) !== -1)) {
@@ -52,16 +51,6 @@ const tranlationService = {
     });
     return jsonObj;
   },
-  downloadFile(data, filename) {
-    const a = window.document.createElement('a');
-    a.href = window.URL.createObjectURL(new Blob([JSON.stringify(data)], { type: 'application/json' }));
-    a.download = 'en_' + filename;
-    // Append anchor to body.
-    document.body.appendChild(a);
-    a.click();
-    // Remove anchor from body
-    document.body.removeChild(a);
-  },
   IsJsonString(str) {
     try {
       var json = JSON.parse(str);
@@ -74,57 +63,99 @@ const tranlationService = {
     $("#fileInput").on("change", function(evt) {
       //capture the file information.
       function handleFile(file) {
-        const new_zip = new JSZip();
+        let new_zip = new JSZip();
         //read the Blob
         new_zip.loadAsync(file)
           .then(function(zip) {
-            //read the entries
-            zip.forEach(function(relativePath, zipEntry) {
-              if (zipEntry.name.indexOf('_en') !== -1) {
-                //only english file
-                new_zip.file(zipEntry.name).async("string").then(function(value) {
-                  if (tranlationService.IsJsonString(value)) {
-                    let fileData = JSON.parse(value);
+            //refine the zip files 
+            Object.keys(zip.files).forEach(function(key) {
+              if (key.indexOf('__') !== -1) delete zip.files[key];
+            });
 
-                    //conver the strings into translation JSON
-                    let array = [];
-                    fileData.forEach(function(index, el) {
-                      let dataObj = { originalHTML: {} };
-                      tranlationService.sentenceArray = [];
-                      for (var key in index) {
-                        let parsedHTML = [];
-                        if (key.indexOf('brightspot') === -1) {
-                          //parse the HTML
-                          if (typeof(index[key]) === 'object') {
-                            dataObj.originalHTML[key] = parsedHTML = parse(index[key][0]);
-                          } else {
-                            dataObj.originalHTML[key] = parsedHTML = parse(index[key]);
-                          }
-                          //get the translation JSON
-                          const translationJSON = tranlationService.createJSON(parsedHTML);
-                          dataObj = {
-                            ...dataObj,
-                            [key]: translationJSON
-                          };
+            //read the entries and reset the count
+            tranlationService.count = 0;
+            let fileZip = new JSZip();
+            zip.forEach(function(relativePath, zipEntry) {
+              new_zip.file(zipEntry.name).async("string").then(function(value) {
+                // read the file increase the count
+                tranlationService.count++;
+
+                let originalMetaData = { html: {} };
+                if (tranlationService.IsJsonString(value)) {
+                  let fileData = JSON.parse(value);
+
+                  //conver the strings into translation JSON
+                  let array = [];
+
+                  fileData.forEach(function(index, el) {
+                    let dataObj = { originalHTML: {} };
+                    let fileObj = {};
+                    tranlationService.sentenceArray = [];
+                    for (var key in index) {
+                      //read the html values in the keys
+                      let parsedHTML = [];
+                      if (key.indexOf('brightspot') === -1) {
+                        //parse the HTML
+                        if (typeof(index[key]) === 'object') {
+                          dataObj.originalHTML = parsedHTML = parse(index[key][0]);
                         } else {
-                          dataObj = {
-                            ...dataObj,
-                            [key]: index[key]
-                          };
+                          dataObj.originalHTML = parsedHTML = parse(index[key]);
                         }
+                        //get the translation JSON
+                        const translationJSON = tranlationService.createJSON(parsedHTML);
+                        dataObj = {
+                          ...dataObj,
+                          [key]: translationJSON
+                        };
+                        fileObj = {
+                          ...fileObj,
+                          [key]: dataObj.originalHTML
+                        };
+                      } else {
+                        dataObj = {
+                          ...dataObj,
+                          [key]: index[key]
+                        };
                       }
-                      array = [...array, dataObj];
+
+                    }
+                    //collect the meta data and refine the data obj
+                    delete dataObj['originalHTML'];
+
+                    //final translation data and meta data
+                    array = [...array, dataObj];
+                    originalMetaData = [...originalMetaData, {
+                      html: fileObj,
+                      meta: dataObj['brightspot.metadata']
+                    }]
+                  });
+
+                  //create the files for the bundle
+                  const arrStrings = JSON.stringify(array);
+                  const metaData = JSON.stringify(originalMetaData);
+
+                  fileZip.file('translation_' + zipEntry.name, arrStrings);
+                  fileZip.file('translation_metaData.json', metaData);
+                }
+                if (tranlationService.count === Object.keys(zip.files).length - 1) {
+                  //wait for promise to be resolved and create the bundle
+                  setTimeout(function() {
+                    fileZip.generateAsync({
+                      type: "blob"
+                    }).then(function(content) {
+                      saveAs(content, "translation_bundle.zip");
+                    }, function(err) {
+                      console.log("err", err);
                     });
-                    //download the json file
-                    tranlationService.downloadFile(array, zipEntry.name);
-                  }
-                });
-              }
+                  }, 500)
+                }
+              });
             });
           }, function(e) {
             console.log("err", e.message);
           });
       }
+      //read the input bundle
       const files = evt.target.files;
       for (let i = 0; i < files.length; i++) {
         handleFile(files[i]);
@@ -132,11 +163,12 @@ const tranlationService = {
     });
   },
   createHTML(htmlData, translatedObj) {
-    let translatedHTML;
+    let translatedHTML ='';
     //store the parsed HTML array length
     for (var i = 0; i < htmlData.length; i++) {
       if (htmlData[i].type === 'element') {
         tranlationService.createHTML(htmlData[i].children, translatedObj);
+        break;
       } else if (htmlData[i].type === 'text') {
         htmlData[i].content = translatedObj;
       }
@@ -148,68 +180,100 @@ const tranlationService = {
     $("#bundleInput").on("change", function(evt) {
       //capture the file information.
       function handleZip(file) {
-        const new_zip = new JSZip();
-        const bundle_zip = new JSZip();
+        let new_zip = new JSZip();
 
         //read the Blob
         new_zip.loadAsync(file)
           .then(function(zip) {
-            //read the entries
-            tranlationService.count = 0;
-            let fileZip = new JSZip();
+            let metaDataFile = {};
+            let metaDataValue = {};
+
+            //refine the zip files 
+            Object.keys(zip.files).forEach(function(key) {
+              if (key.indexOf('__') !== -1) delete zip.files[key];
+              if (key.indexOf('json') === -1) delete zip.files[key];
+            });
+
+            //find the metadata file in the bundle
             zip.forEach(function(relativePath, zipEntry) {
-              if ((relativePath === zipEntry.name) && (zipEntry.name.indexOf('json') !== -1)) {
-                new_zip.file(zipEntry.name).async("string").then(function(value) {
-                  tranlationService.count++;
-
-                  function joinString(obj) {
-                    let string = '';
-                    for (var key in obj) {
-                      string = string + obj[key];
-                    }
-                    return string;
-                  }
-                  if (tranlationService.IsJsonString(value)) {
-                    const fileData = JSON.parse(value);
-                    //convert the translated strings into json
-                    let array = [];
-                    fileData.forEach(function(index, el) {
-                      let dataObj = {};
-                      for (var key in index) {
-                        if (key.indexOf('brightspot') !== -1) {
-                          dataObj = {
-                            ...dataObj,
-                            [key]: index[key]
-                          };
-                        } else if (key !== 'originalHTML') {
-                          dataObj = {
-                            ...dataObj,
-                            [key]: tranlationService.createHTML(index.originalHTML[key], joinString(index[key]))
-                          };
-                        }
-                      }
-                      array = [...array, dataObj];
-                    });
-
-                    const arrStrings = JSON.stringify(array);
-                    fileZip.file(zipEntry.name, arrStrings);
-                  }
-                  if (tranlationService.count === Object.keys(zip.files).length - 1) {
-                    fileZip.generateAsync({
-                      type: "blob"
-                    }).then(function(content) {
-                      saveAs(content, "bundle.zip");
-                    }, function(err) {
-                      console.log("err", err);
-                    });
-                  }
-                });
+              if (zipEntry.name.indexOf('meta') !== -1) {
+                metaDataFile = zipEntry;
               }
+            });
+            //read the metadata file
+            new_zip.file(metaDataFile.name).async("string").then(function(value) {
+              metaDataValue = JSON.parse(value);
+
+              //reset the count and read the entries
+              tranlationService.count = 0;
+              let fileZip = new JSZip();
+              zip.forEach(function(relativePath, zipEntry) {
+                //read all the json files except the meta data file
+                if (zipEntry.name.indexOf('meta') === -1) {
+                  new_zip.file(zipEntry.name).async("string").then(function(value) {
+                    //read the file and increase the count
+                    tranlationService.count++;
+
+                    function joinString(obj) {
+                      let string = '';
+                      for (var key in obj) {
+                        string = string + obj[key];
+                      }
+                      return string;
+                    }
+
+                    if (tranlationService.IsJsonString(value)) {
+                      const fileData = JSON.parse(value);
+
+                      //convert the translated strings into json
+                      let array = [];
+                      fileData.forEach(function(index, el) {
+                        //map the trasnlated content with orginal html through articleID in metadata
+                        const articleId = index['brightspot.metadata'].articleId;
+
+                        for (var i = metaDataValue.length - 1; i >= 0; i--) {
+                          if (metaDataValue[i].meta.articleId === articleId) {
+                            let dataObj = {};
+                            for (var key in index) {
+                              if (key.indexOf('brightspot') === -1) {
+                                dataObj = {
+                                  ...dataObj,
+                                  [key]: tranlationService.createHTML(metaDataValue[i].html[key], joinString(index[key]))
+                                };
+                              } else {
+                                dataObj = {
+                                  ...dataObj,
+                                  [key]: index[key]
+                                };
+                              }
+                            }
+                            array = [...array, dataObj];
+                            break;
+                          }
+                        }
+                        const arrStrings = JSON.stringify(array);
+                        fileZip.file(zipEntry.name, arrStrings);
+                      });
+                    }
+                    if (tranlationService.count === Object.keys(zip.files).length - 1) {
+
+                      fileZip.generateAsync({
+                        type: "blob"
+                      }).then(function(content) {
+                        saveAs(content, "brightspot_bundle.zip");
+                      }, function(err) {
+                        console.log("err", err);
+                      });
+                    }
+                  });
+                }
+              });
             });
           }, function(e) {
             console.log("err", e.message);
           });
       }
+      //read the input bundle
       const files = evt.target.files;
       for (let i = 0; i < files.length; i++) {
         handleZip(files[i]);
